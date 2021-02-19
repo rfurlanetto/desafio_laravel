6107379c50117c6f54095ed233a6e26081d6683b
rfurlanetto <renecreative13@gmail.com>
2021-02-19 09:34:24 -0300

    Update MovieSeeder.php

--- database/seeds/MovieSeeder.php
+++ database/seeds/MovieSeeder.php
@@ -16,37 +16,37 @@ class MovieSeeder extends Seeder
             [
                 'title' => 'Tenet',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tellus ut sapien fringilla, et dictum dui posuere. Quisque elementum pulvinar dolor a pulvinar. Pellentesque feugiat nunc nec massa pharetra egestas. Etiam non cursus justo, id hendrerit augue. Fusce malesuada quam a sagittis cursus. Pellentesque rhoncus, lorem a sodales congue, ante orci dictum urna, at vehicula neque purus vel diam. Suspendisse sed urna non tortor aliquet mollis in in nisi. Proin nec ipsum faucibus, vestibulum odio a, tristique ipsum. Donec interdum elementum enim, sed lacinia massa gravida nec. Donec at ante maximus, dapibus libero interdum, hendrerit sem. Nam ornare libero at urna blandit elementum. Fusce lectus erat, pellentesque molestie fermentum at, scelerisque pretium ex. Vivamus eleifend vel est a luctus. Cras accumsan semper mauris at varius. Vivamus laoreet, leo finibus venenatis venenatis, elit dui sollicitudin ligula, ac euismod nibh eros eget diam. Cras pellentesque interdum ornare.',
-                'image' => 'index.jpeg',
+                'image' => 'img/index.jpeg',
                 'year' => '2020',
             ],
             [
                 'title' => 'Red Dot',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tellus ut sapien fringilla, et dictum dui posuere. Quisque elementum pulvinar dolor a pulvinar. Pellentesque feugiat nunc nec massa pharetra egestas. Etiam non cursus justo, id hendrerit augue. Fusce malesuada quam a sagittis cursus. Pellentesque rhoncus, lorem a sodales congue, ante orci dictum urna, at vehicula neque purus vel diam. Suspendisse sed urna non tortor aliquet mollis in in nisi. Proin nec ipsum faucibus, vestibulum odio a, tristique ipsum. Donec interdum elementum enim, sed lacinia massa gravida nec. Donec at ante maximus, dapibus libero interdum, hendrerit sem. Nam ornare libero at urna blandit elementum. Fusce lectus erat, pellentesque molestie fermentum at, scelerisque pretium ex. Vivamus eleifend vel est a luctus. Cras accumsan semper mauris at varius. Vivamus laoreet, leo finibus venenatis venenatis, elit dui sollicitudin ligula, ac euismod nibh eros eget diam. Cras pellentesque interdum ornare.',
-                'image' => 'index2.jpeg',
+                'image' => 'img/index2.jpeg',
                 'year' => '2021',
             ],
             [
                 'title' => 'News world',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tellus ut sapien fringilla, et dictum dui posuere. Quisque elementum pulvinar dolor a pulvinar. Pellentesque feugiat nunc nec massa pharetra egestas. Etiam non cursus justo, id hendrerit augue. Fusce malesuada quam a sagittis cursus. Pellentesque rhoncus, lorem a sodales congue, ante orci dictum urna, at vehicula neque purus vel diam. Suspendisse sed urna non tortor aliquet mollis in in nisi. Proin nec ipsum faucibus, vestibulum odio a, tristique ipsum. Donec interdum elementum enim, sed lacinia massa gravida nec. Donec at ante maximus, dapibus libero interdum, hendrerit sem. Nam ornare libero at urna blandit elementum. Fusce lectus erat, pellentesque molestie fermentum at, scelerisque pretium ex. Vivamus eleifend vel est a luctus. Cras accumsan semper mauris at varius. Vivamus laoreet, leo finibus venenatis venenatis, elit dui sollicitudin ligula, ac euismod nibh eros eget diam. Cras pellentesque interdum ornare.',
-                'image' => 'index3.jpeg',
+                'image' => 'img/index3.jpeg',
                 'year' => '2021',
             ],
             [
                 'title' => 'The Map of Tiny Perfect Things',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tellus ut sapien fringilla, et dictum dui posuere. Quisque elementum pulvinar dolor a pulvinar. Pellentesque feugiat nunc nec massa pharetra egestas. Etiam non cursus justo, id hendrerit augue. Fusce malesuada quam a sagittis cursus. Pellentesque rhoncus, lorem a sodales congue, ante orci dictum urna, at vehicula neque purus vel diam. Suspendisse sed urna non tortor aliquet mollis in in nisi. Proin nec ipsum faucibus, vestibulum odio a, tristique ipsum. Donec interdum elementum enim, sed lacinia massa gravida nec. Donec at ante maximus, dapibus libero interdum, hendrerit sem. Nam ornare libero at urna blandit elementum. Fusce lectus erat, pellentesque molestie fermentum at, scelerisque pretium ex. Vivamus eleifend vel est a luctus. Cras accumsan semper mauris at varius. Vivamus laoreet, leo finibus venenatis venenatis, elit dui sollicitudin ligula, ac euismod nibh eros eget diam. Cras pellentesque interdum ornare.',
-                'image' => 'index4.jpeg',
+                'image' => 'img/index4.jpeg',
                 'year' => '2021',
             ],
             [
                 'title' => 'Resgate',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tellus ut sapien fringilla, et dictum dui posuere. Quisque elementum pulvinar dolor a pulvinar. Pellentesque feugiat nunc nec massa pharetra egestas. Etiam non cursus justo, id hendrerit augue. Fusce malesuada quam a sagittis cursus. Pellentesque rhoncus, lorem a sodales congue, ante orci dictum urna, at vehicula neque purus vel diam. Suspendisse sed urna non tortor aliquet mollis in in nisi. Proin nec ipsum faucibus, vestibulum odio a, tristique ipsum. Donec interdum elementum enim, sed lacinia massa gravida nec. Donec at ante maximus, dapibus libero interdum, hendrerit sem. Nam ornare libero at urna blandit elementum. Fusce lectus erat, pellentesque molestie fermentum at, scelerisque pretium ex. Vivamus eleifend vel est a luctus. Cras accumsan semper mauris at varius. Vivamus laoreet, leo finibus venenatis venenatis, elit dui sollicitudin ligula, ac euismod nibh eros eget diam. Cras pellentesque interdum ornare.',
-                'image' => 'index5.jpeg',
+                'image' => 'img/index5.jpeg',
                 'year' => '2020',
             ],
             [
                 'title' => 'Soul',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tellus ut sapien fringilla, et dictum dui posuere. Quisque elementum pulvinar dolor a pulvinar. Pellentesque feugiat nunc nec massa pharetra egestas. Etiam non cursus justo, id hendrerit augue. Fusce malesuada quam a sagittis cursus. Pellentesque rhoncus, lorem a sodales congue, ante orci dictum urna, at vehicula neque purus vel diam. Suspendisse sed urna non tortor aliquet mollis in in nisi. Proin nec ipsum faucibus, vestibulum odio a, tristique ipsum. Donec interdum elementum enim, sed lacinia massa gravida nec. Donec at ante maximus, dapibus libero interdum, hendrerit sem. Nam ornare libero at urna blandit elementum. Fusce lectus erat, pellentesque molestie fermentum at, scelerisque pretium ex. Vivamus eleifend vel est a luctus. Cras accumsan semper mauris at varius. Vivamus laoreet, leo finibus venenatis venenatis, elit dui sollicitudin ligula, ac euismod nibh eros eget diam. Cras pellentesque interdum ornare.',
-                'image' => 'index6.jpeg',
+                'image' => 'img/index6.jpeg',
                 'year' => '2020',
             ]
         ];
